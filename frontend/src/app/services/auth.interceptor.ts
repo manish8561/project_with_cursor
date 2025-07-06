e361acaa7c2784678d4979b1557822f7d7f73bd4
Manish Sharma <manish198646@yahoo.com>
2025-07-07 00:05:03 +0530

    fixing interceptor

--- frontend/src/app/services/auth.interceptor.ts
+++ frontend/src/app/services/auth.interceptor.ts
@@ -10,7 +10,7 @@ export class AuthInterceptor implements HttpInterceptor {
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         const token = this.authService.getToken();
-
+        console.log("token", token)
         if (token) {
             const cloned = req.clone({
                 headers: req.headers.set('Authorization', `Bearer ${token}`)
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
+import { AuthInterceptor } from './app/services/auth.interceptor';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(
+      withInterceptors([AuthInterceptor])
+    ),
+    // ...other providers
+  ]
+})
   .catch((err) => console.error(err));
