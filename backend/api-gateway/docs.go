4d234b6d0e25097374dd4838f21c00010f90d53a
Manish Sharma <manish198646@yahoo.com>
2025-08-08 10:06:56 +0530

    fix

--- backend/api-gateway/docs.go
+++ backend/api-gateway/docs.go
@@ -42,8 +42,46 @@ import "github.com/swaggo/swag"
 // @description Type "Bearer" followed by a space and JWT token.
 
 func init() {
+	docTemplate := `{
+	"swagger": "2.0",
+	"info": {
+		"description": "API Gateway for microservices architecture",
+		"title": "API Gateway",
+		"termsOfService": "http://swagger.io/terms/",
+		"contact": {
+			"name": "API Support",
+			"url": "http://www.swagger.io/support",
+			"email": "support@swagger.io"
+		},
+		"license": {
+			"name": "Apache 2.0",
+			"url": "http://www.apache.org/licenses/LICENSE-2.0.html"
+		},
+		"version": "1.0"
+	},
+	"host": "localhost:8080",
+	"basePath": "/",
+	"schemes": [
+		"http",
+		"https"
+	],
+	"consumes": [
+		"application/json"
+	],
+	"produces": [
+		"application/json"
+	],
+	"securityDefinitions": {
+		"BearerAuth": {
+			"type": "apiKey",
+			"name": "Authorization",
+			"in": "header",
+			"description": "Type \"Bearer\" followed by a space and JWT token."
+		}
+	}
+}`
 	swag.Register(swag.Name, &swag.Spec{
 		InfoInstanceName: "swagger",
-		SwaggerTemplate: docTemplate,
+		SwaggerTemplate:  docTemplate,
 	})
-} 
+}
